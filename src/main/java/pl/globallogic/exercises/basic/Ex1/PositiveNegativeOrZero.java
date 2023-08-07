a61b7281a48224c3693530db01b312666db303fb
Katarzyna Bak <katarzyna.bak@globallogic.com>
2023-08-07 04:51:00 +0200

    ex 1 done

--- src/main/java/pl/globallogic/exercises/basic/Ex1/PositiveNegativeOrZero.java
+++ src/main/java/pl/globallogic/exercises/basic/Ex1/PositiveNegativeOrZero.java
@@ -1,2 +1,17 @@
-package pl.globallogic.exercises.basic.Ex1;public class PositiveNegativeOrZero {
+package pl.globallogic.exercises.basic.Ex1;
+
+import pl.globallogic.Main;
+
+public class PositiveNegativeOrZero {
+    public static void CheckNum(int num) {
+        if( num < 0 ) System.out.println("The number is negative");
+        else if (num > 0) System.out.println("The number is positive");
+        else System.out.println("The number is equal to 0");
+    }
+
+    public static void main(String[] args) {
+        CheckNum(2);
+        CheckNum(0);
+        CheckNum(-3);
+    }
 }
