32ff0aafb4bbafa8dd442196930dd842eb190454
Katarzyna Bak <katarzyna.bak@globallogic.com>
2023-08-07 03:57:23 +0200

    commit

---
+++ src/main/java/pl/globallogic/lessons/lesson1/PrimitiveTypesSandbox.java
@@ -0,0 +1,19 @@
+package pl.globallogic.lessons.lesson1;
+
+public class PrimitiveTypesSandbox {
+
+    public static void main(String[] args) {
+        int number = 340;
+        byte smallNumber = (byte)number;
+        long bigbigNumber = 32432423;
+        float floatNumber = 3.14574654F;
+        char symbol = 'b';
+        boolean isJavaEasy;
+        String firstName = "John Doe";
+        int sum = 23 + 567;
+        int diff = 4546 - 5857;
+        int idx = 1;
+        System.out.println(idx++);
+        System.out.println(idx);
+    }
+}
