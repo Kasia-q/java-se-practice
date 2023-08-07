9218a7e438e18173757d5bae3b7f17031175ba39
Katarzyna Bak <katarzyna.bak@globallogic.com>
2023-08-07 05:23:17 +0200

    ex2 update and change in ex1

--- src/main/java/pl/globallogic/exercises/basic/Ex1PositiveNegativeOrZero.java
+++ src/main/java/pl/globallogic/exercises/basic/Ex1PositiveNegativeOrZero.java
@@ -1,15 +1,17 @@
 package pl.globallogic.exercises.basic;
 
 public class Ex1PositiveNegativeOrZero {
-    public static void CheckNum(int num) {
-        if( num < 0 ) System.out.println("The number is negative");
-        else if (num > 0) System.out.println("The number is positive");
-        else System.out.println("The number is equal to 0");
-    }
 
     public static void main(String[] args) {
         CheckNum(2);
         CheckNum(0);
         CheckNum(-3);
     }
+    public static void CheckNum(int num) {
+        if( num < 0 ) System.out.println("The number is negative");
+        else if (num > 0) System.out.println("The number is positive");
+        else System.out.println("The number is equal to 0");
+    }
+
+
 }
--- src/main/java/pl/globallogic/exercises/basic/Ex2SpeedConverter.java
+++ src/main/java/pl/globallogic/exercises/basic/Ex2SpeedConverter.java
@@ -1,4 +1,27 @@
 package pl.globallogic.exercises.basic;
 
 public class Ex2SpeedConverter {
+//    public static void main(String[] args) {
+//        System.out.println(toMilesPerHour(kilometersPerHour : 1.5) == 1);
+//        System.out.println(toMilesPerHour(kilometersPerHour : 10.25) == 6);
+//        System.out.println(toMilesPerHour(kilometersPerHour : -5.6) == -1);
+//    }
+
+
+    private static String convertedSpeedMessage(double kilometersPerHour) {
+        long miles = toMilesPerHour(kilometersPerHour);
+        if(miles < 0) {
+            return "Invalid value";
+        }
+        return kilometersPerHour + " km/h = " + miles + " mi/h";
+    }
+
+    private static long toMilesPerHour(double kilometersPerHour){
+        double conversionCoefficient = 1.609;
+        if(kilometersPerHour < 0)
+            return -1;
+        return Math.round(kilometersPerHour / conversionCoefficient);
+    }
+
+
 }
