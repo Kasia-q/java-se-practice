70f89db7d74a772867048fbbd5d9b69bef7b4bb5
Katarzyna Bak <katarzyna.bak@globallogic.com>
2023-08-07 05:34:59 +0200

    fixed and finished ex2

--- src/main/java/pl/globallogic/exercises/basic/Ex2SpeedConverter.java
+++ src/main/java/pl/globallogic/exercises/basic/Ex2SpeedConverter.java
@@ -1,11 +1,17 @@
 package pl.globallogic.exercises.basic;
 
 public class Ex2SpeedConverter {
-//    public static void main(String[] args) {
-//        System.out.println(toMilesPerHour(kilometersPerHour : 1.5) == 1);
-//        System.out.println(toMilesPerHour(kilometersPerHour : 10.25) == 6);
-//        System.out.println(toMilesPerHour(kilometersPerHour : -5.6) == -1);
-//    }
+    public static void main(String[] args) {
+        System.out.println(toMilesPerHour( 1.5) == 1);
+        System.out.println(toMilesPerHour( 10.25) == 6);
+        System.out.println(toMilesPerHour( -5.6) == -1);
+        System.out.println(convertedSpeedMessage(1.5).equals("1.5 km/h = 1 mi/h"));
+        System.out.println(convertedSpeedMessage(25.42).equals("25.42 km/h = 16 mi/h"));
+        System.out.println(convertedSpeedMessage(-7.89).equals("Invalid value"));
+
+        System.out.println(convertedSpeedMessage(1.5));
+        System.out.println(convertedSpeedMessage(-5.6));
+    }
 
 
     private static String convertedSpeedMessage(double kilometersPerHour) {
