f37555d5d1ab82483d97604b90dabaf2f3831b5b
Katarzyna Bak <katarzyna.bak@globallogic.com>
2023-08-08 17:04:21 +0200

    ex8 done

---
+++ src/main/java/pl/globallogic/exercises/basic/Ex8TeenNumberChecker.java
@@ -0,0 +1,15 @@
+package pl.globallogic.exercises.basic;
+
+public class Ex8TeenNumberChecker {
+    public static void main(String[] args){
+        System.out.println(hasTeen(9,99,19));
+        System.out.println(hasTeen(23,15,42));
+        System.out.println(hasTeen(22,23,34));
+        System.out.println(hasTeen(13,19,22));
+
+    }
+    private static boolean hasTeen(int age1, int age2, int age3){
+        if((age1 >= 13 && age1 <= 19) || (age2 >= 13 && age2 <= 19) || (age3 >= 13 && age3 <= 19)) return true;
+        return false;
+    }
+}
