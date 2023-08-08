3988e0964beca43325a5c18ae671eb6714cff665
Katarzyna Bak <katarzyna.bak@globallogic.com>
2023-08-08 16:40:56 +0200

    ex7 done

--- src/main/java/pl/globallogic/exercises/basic/Ex7EqualSumChecker.java
+++ src/main/java/pl/globallogic/exercises/basic/Ex7EqualSumChecker.java
@@ -2,9 +2,17 @@ package pl.globallogic.exercises.basic;
 
 public class Ex7EqualSumChecker {
     public static void main(String[] args){
+        System.out.println(hasEqualSum(1,1,1));
+        System.out.println(hasEqualSum(1,1,2));
+        System.out.println(hasEqualSum(1,-1,0));
+        System.out.println(hasEqualSum(2,2,4));
 
     }
     private static boolean hasEqualSum(int num1, int num2, int sum){
+        if (num1 + num2 == sum) {
+            return true;
+        }
 
+        return false;
     }
 }
