b7b30db1c2aeabcda48cc61e78895bc407a051fd
Katarzyna Bak <katarzyna.bak@globallogic.com>
2023-08-08 16:09:13 +0200

    start of ex 7

--- src/main/java/pl/globallogic/exercises/basic/Ex7EqualSumChecker.java
+++ src/main/java/pl/globallogic/exercises/basic/Ex7EqualSumChecker.java
@@ -1,2 +1,10 @@
-package pl.globallogic.exercises.basic;public class Ex7EqualSumChecker {
+package pl.globallogic.exercises.basic;
+
+public class Ex7EqualSumChecker {
+    public static void main(String[] args){
+
+    }
+    private static boolean hasEqualSum(int num1, int num2, int sum){
+
+    }
 }
