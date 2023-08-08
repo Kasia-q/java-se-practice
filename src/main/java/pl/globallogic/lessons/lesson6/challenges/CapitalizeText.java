54946003ffccd4d8951038840a4814a10d89620c
Katarzyna Bak <katarzyna.bak@globallogic.com>
2023-08-08 16:26:56 +0200

    lesson update

--- src/main/java/pl/globallogic/lessons/lesson6/challenges/CapitalizeText.java
+++ src/main/java/pl/globallogic/lessons/lesson6/challenges/CapitalizeText.java
@@ -2,7 +2,27 @@ package pl.globallogic.lessons.lesson6.challenges;
 
 public class CapitalizeText {
     public static void main(String[] args){
+        String targetText = "i love java programming";
+        String expectedResult = "I Love Java Programming";
+        System.out.println(expectedResult.contentEquals(process(targetText)));
 
+    }
+
+    private static String process(String targetText){
+        char[] textAsChars = targetText.toCharArray();
+        boolean capitalizeNextChar = true;
+        for (int i = 0; i < textAsChars.length; i++) {
+            char currentChar = textAsChars[i];
+            if (Character.isWhitespace(currentChar)){
+                capitalizeNextChar = true;
+            } else {
+                if (capitalizeNextChar && Character.isLetter(currentChar)){
+                    textAsChars[i] = Character.toUpperCase(currentChar);
+                    capitalizeNextChar = false;
+                }
+            }
+        }
+        return new String(textAsChars);
     }
 
 }
